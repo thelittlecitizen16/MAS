ced99fc9338a871a6363740099fc48601bc7c75d
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-08-25 17:44:18 +0300

    manage agents

---
+++ MAS/NewFolder/ManageAgents.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AgentsProject.Agents;
+using AgentsProject.Interfaces;
+
+namespace MAS.NewFolder
+{
+    public class ManageAgents
+    {
+        public List<IAgent> allAgents { get; private set; }
+
+        public ManageAgents()
+        {
+            allAgents = new List<IAgent>();
+            AddAllAgents();
+        }
+
+        public void RegisterAgent(IAgent agent)
+        {
+            allAgents.Add(agent);
+        }
+
+        private void AddAllAgents()
+        {
+            allAgents.Add(new MamasAgent());
+            allAgents.Add(new BamasAgent());
+            allAgents.Add(new LaLaLandAgent());
+        }
+
+
+    }
+}
