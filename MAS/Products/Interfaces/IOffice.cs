4c60857ac29a29cd20962fb2ddf3f484c0d8af31
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-08-25 17:28:18 +0300

    add products item office 1 and 2

--- MAS/Products/Interfaces/IOffice.cs
+++ MAS/Products/Interfaces/IOffice.cs
@@ -4,7 +4,8 @@ using System.Text;
 
 namespace MAS.Products.Interfaces
 {
-    interface IOffice
+    public interface IOffice : IProduct
     {
+        int NumberOfRooms { get; }
     }
 }
---
+++ MAS/Products/Items/Office1.cs
@@ -0,0 +1,21 @@
+﻿using MAS.Products.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MAS.Products.Items
+{
+    public class Office1 : IOffice
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public int NumberOfRooms { get; private set; }
+
+        public Office1()
+        {
+            Name = "Office one";
+            Description = "main building office number 1";
+            NumberOfRooms = 3;
+        }
+    }
+}
