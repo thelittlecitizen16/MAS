4482f7e0ecbbf65c26871ed225e0ac11941e096b
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-08-25 20:18:44 +0300

    manage auction by add to event

--- MAS/ManageAuction.cs
+++ MAS/ManageAuction.cs
@@ -12,6 +12,12 @@ namespace MAS
         private Auction _auction;
         private ManageAgents _manageAgents;
         private List<IAgent> _agentInAuction;
+        private IAgent _lastAgentOffer;
+        private double _lastOfferPrice;
+        private event Func<double> FirstOffer;
+        private event Func<string, double,  double> NewOffer;
+        private event Func<string, double, double> LastOffer;
+
 
         public ManageAuction(Auction auction)
         {
@@ -40,6 +46,26 @@ namespace MAS
             Console.WriteLine($" the product is {_auction.Product} the start price is {_auction.StartPrice}");
         }
 
+        public void SendIfWantToAddFirstOffer(IAgent agent)
+        {
+            FirstOffer += agent.FirstOffer;
+        }
 
+        public void SendIfWantToAddNewOffer(IAgent agent)
+        {
+            NewOffer += agent.NewOffer;
+        }
+
+        public void LastChance(IAgent agent)
+        {
+            LastOffer += agent.OfferLastChance;
+        }
+
+        public void EndAuction(IAgent agent)
+        {
+            Console.WriteLine($"the auction over-");
+            Console.WriteLine($"{_lastAgentOffer.Name} is the winner and he buy in {_lastOfferPrice}");
+
+        }
     }
 }
