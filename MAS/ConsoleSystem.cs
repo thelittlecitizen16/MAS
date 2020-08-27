60b6a40c9650b354d672ea15e312b4f6146d1358
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-08-27 09:31:19 +0300

    add system classes

---
+++ MAS/ConsoleSystem.cs
@@ -0,0 +1,20 @@
+﻿using MAS.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MAS
+{
+    public class ConsoleSystem : ISystem
+    {
+        public string ReadString()
+        {
+            return Console.ReadLine();
+        }
+
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
